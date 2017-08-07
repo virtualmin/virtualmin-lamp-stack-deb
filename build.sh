69d9f3275afb99cef9094aa041b498f9689d7559
Joe Cooper <swelljoe@gmail.com>
2017-08-07 05:34:13 -0500

    Generate LEMP files and clean them up when building

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 # Builds all of the metapackages in this directory.
 # Requires equivs-build and dpkg-buildpackage to be in the path.
+
+# Create LEMP from LAMP files
+./generate-lemp.sh
+
 for i in *.ctl; do
 	equivs-build $i
 done
+
+# Remove LEMP files
+rm *lemp*.ctl
+
