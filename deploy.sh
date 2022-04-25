605ce7bb9bb937cc7dd91780d5e1000643e1713c
Joe Cooper <joe@virtualmin.com>
2022-04-25 03:46:31 +0000

    Move to virtualmin-7 repos

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 # Script to add packages to aptly repos
 
-for i in *deb-8*.deb; do
-  echo "Adding $i to jessie repo"
-  aptly repo add virtualmin-jessie $i
-done
-
-for i in *deb-9*.deb; do
-  echo "Adding $i to stretch repo"
-  aptly repo add virtualmin-stretch $i
-done
-
 for i in *deb-10*.deb; do
   echo "Adding $i to buster repo"
-  aptly repo add virtualmin-buster $i
+  aptly repo add virtualmin-7-buster $i
 done
 
+for i in *deb-11*.deb; do
+  echo "Adding $i to bullseye repo"
+  aptly repo add virtualmin-7-bullseye $i
+done
